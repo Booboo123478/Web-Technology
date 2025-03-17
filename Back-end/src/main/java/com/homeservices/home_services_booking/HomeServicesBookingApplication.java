b6dc670c8c0ee4a8c5e69e7846993584bc224b05
Booboo123478 <marcantoinetissot7@gmail.com>
2025-03-17 16:44:31 +0100

    Ini back

---
+++ Back-end/src/main/java/com/homeservices/home_services_booking/HomeServicesBookingApplication.java
@@ -0,0 +1,13 @@
+package com.homeservices.home_services_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServicesBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeServicesBookingApplication.class, args);
+	}
+
+}
