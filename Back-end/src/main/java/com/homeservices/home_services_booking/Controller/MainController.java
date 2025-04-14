27f0dc72bad419592277bf68b4ed3b810f294b86
Etiennedctey <151937365+Etiennedctey@users.noreply.github.com>
2025-04-14 16:56:44 +0200

    more header

--- Back-end/src/main/java/com/homeservices/home_services_booking/Controller/MainController.java
+++ Back-end/src/main/java/com/homeservices/home_services_booking/Controller/MainController.java
@@ -9,7 +9,7 @@ public class MainController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("header", "Header/header :: header");
+        model.addAttribute("header", "header :: header");
         return "index";
     }
 }
